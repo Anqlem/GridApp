7a9be69bd9833e9de1df5cf431b2760efc8c4e57
Миша Шмелёв <cryptecker@hotmail.com>
2020-10-14 01:14:56 +0300

    TicTacToe

--- GridApp/GridApp/MainPage.xaml.cs
+++ GridApp/GridApp/MainPage.xaml.cs
@@ -11,46 +11,244 @@ namespace GridApp
 {
     public partial class MainPage : ContentPage
     {
-        BoxView box;
+        Image img;
+        Grid ground;
+        Button restartbtn, clearbutton;
+        Label CrossOrZero;
+        static int player;
+        int[,] WinOrLose = new int[3, 3];
+        static int flag = 0;
         public MainPage()
         {
-            Grid grid = new Grid();
+            PlayGround();
+            BackgroundColor = Color.FromRgb(192, 179, 223);
+
+
+
+
+
+            restartbtn = new Button()
+            {
+                Text = "Who's first?",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            restartbtn.Clicked += RestartbtnOnClicked;
+
+            clearbutton = new Button()
+            {
+                Text = "New Game",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            clearbutton.Clicked += ClearbuttonOnClicked;
+
+            StackLayout BtnLayout = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { restartbtn, clearbutton }
+            };
+
+            CrossOrZero = new Label()
+            {
+                TextColor = Color.White
+            };
+
+            StackLayout MainLayout = new StackLayout()
+            {
+                Children = { ground, BtnLayout, CrossOrZero }
+            };
+
+            Content = MainLayout;
+        }
+        private void PlayGround()
+        {
+            ground = new Grid()
+            {
+                HeightRequest = 400
+            };
             for (int i = 0; i < 3; i++)
             {
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                ground.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                ground.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
-            
-   
+
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    box = new BoxView { Color = Color.Blue };
-                    grid.Children.Add(box,i,j);
+                    img = new Image
+                    {
+
+                        HeightRequest = 125,
+                        Source = "empty.png"
+                    };
+                    WinOrLose[i, j] = 0;
                     var tap = new TapGestureRecognizer();
-                    tap.Tapped += Tap_Tapped;
-                    box.GestureRecognizers.Add(tap);
+                    tap.Tapped += TapOnTapped;
+                    ground.Children.Add(img, i, j);
+                    img.GestureRecognizers.Add(tap);
                 }
             }
-            Content = grid;
         }
 
-        List<BoxView> clicked = new List<BoxView> { };
-        private void Tap_Tapped(object sender, EventArgs e)
+        private void ClearbuttonOnClicked(object sender, EventArgs e)
         {
-            BoxView boxView = sender as BoxView;
-            if (clicked.Contains(boxView))
+            for (int i = 0; i < 3; i++)
             {
-                boxView.Color = Color.Red;
-                clicked.Remove(boxView);
+                for (int j = 0; j < 3; j++)
+                {
+                    img = new Image
+                    {
+                        HeightRequest = 125,
+                        Source = "empty.png"
+                    };
+                    var tap = new TapGestureRecognizer();
+                    tap.Tapped += TapOnTapped;
+                    ground.Children.Add(img, i, j);
+                    img.GestureRecognizers.Add(tap);
+                }
+            }
+            restartbtn.Opacity = 1;
+            player = 0;
+            flag = 0;
+
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    WinOrLose[y, x] = 0;
+                }
+            }
+        }
+
+        private void RestartbtnOnClicked(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            player = rnd.Next(1, 3);
+            if (player == 1)
+            {
+                DisplayAlert("Who's first?", "X Starts", "Ок");
             }
             else
             {
-                boxView.Color = Color.Blue;
-                clicked.Add(boxView);
+                DisplayAlert("Who's first?", "O Starts", "Ок");
+            }
+            restartbtn.Opacity = 0;
+
+        }
+
+        private void TapOnTapped(object sender, EventArgs e)
+        {
+            Image img = sender as Image;
+            var imageSource = (Image)sender;
+            var selectedImage = imageSource.Source as FileImageSource;
+
+            if (selectedImage == "empty.png")
+            {
+                if (player == 1)
+                {
+                    img.Source = ImageSource.FromFile("krestik.png");
+                    WinOrLose[Grid.GetRow(img), Grid.GetColumn(img)] = 1;
+                    player = 2;
+                    WinCheck();
+                    CheckTheWinner();
+                }
+                else if (player == 2)
+                {
+                    img.Source = ImageSource.FromFile("nolik.png");
+                    WinOrLose[Grid.GetRow(img), Grid.GetColumn(img)] = 2;
+                    player = 1;
+                    WinCheck();
+                    CheckTheWinner();
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "Occupied", "Ок");
+            }
+        }
+        public int WinCheck()
+        {
+            if (WinOrLose[0, 0] == 1 && WinOrLose[0, 1] == 1 && WinOrLose[0, 2] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[1, 0] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[1, 2] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[2, 0] == 1 && WinOrLose[2, 1] == 1 && WinOrLose[2, 2] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[0, 0] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 2] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[0, 2] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 0] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[0, 0] == 1 && WinOrLose[1, 0] == 1 && WinOrLose[2, 0] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[0, 1] == 1 && WinOrLose[1, 1] == 1 && WinOrLose[2, 1] == 1)
+            {
+                flag = 1;
+            }
+            else if (WinOrLose[0, 2] == 1 && WinOrLose[1, 2] == 1 && WinOrLose[2, 2] == 1)
+            {
+                flag = 1;
             }
 
+            //2
+            else if (WinOrLose[0, 0] == 2 && WinOrLose[0, 1] == 2 && WinOrLose[0, 2] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[1, 0] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[1, 2] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[2, 0] == 2 && WinOrLose[2, 1] == 2 && WinOrLose[2, 2] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[0, 0] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 2] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[0, 2] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 0] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[0, 0] == 2 && WinOrLose[1, 0] == 2 && WinOrLose[2, 0] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[0, 1] == 2 && WinOrLose[1, 1] == 2 && WinOrLose[2, 1] == 2)
+            {
+                flag = 2;
+            }
+            else if (WinOrLose[0, 2] == 2 && WinOrLose[1, 2] == 1 && WinOrLose[2, 2] == 2)
+            {
+                flag = 2;
+            }
+
+            return flag;
+        }
+        public void CheckTheWinner()
+        {
+            flag = WinCheck();
+
+            if (flag == 1)
+            {
+                DisplayAlert("Victory!", "X Won", "Ок");
+            }
+            else if (flag == 2)
+            {
+                DisplayAlert("Victory!", "O Won", "Ок");
+            }
         }
     }
 }
